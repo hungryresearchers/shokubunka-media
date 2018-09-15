b6031a981166a7e169dc56ec4fee24c8c655a6e3
yasuno0327 <5315076@ed.tus.ac.jp>
2018-09-15 23:11:43 +0900

    Setup qor admin

--- api/infrastructure/router.go
+++ api/infrastructure/router.go
@@ -1,18 +1,29 @@
 package infrastructure
 
 import (
+	"api/domain"
 	"api/interfaces/controllers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/qor/admin"
 )
 
 var Router *gin.Engine
 
 func init() {
 	router := gin.Default()
+	mux := http.NewServeMux()
 
 	sqlHandler := NewSqlHandler()
 	Migrate(sqlHandler.Conn)
+
+	// Admin config & routing
+	Admin := admin.New(&admin.AdminConfig{DB: sqlHandler.Conn})
+	Admin.AddResource(&domain.User{})
+	Admin.MountTo("/admin", mux)
+	router.Any("/admin/*resources", gin.WrapH(mux))
+
 	// controller
 	userController := controllers.NewUserController(sqlHandler)
 
