cce2b2acbf00ebc419021eff58291bd12e64c353
yasuno0327 <5315076@ed.tus.ac.jp>
2018-09-26 22:00:42 +0900

    add validator

--- api/domain/article.go
+++ api/domain/article.go
@@ -3,13 +3,13 @@ package domain
 import "time"
 
 type Article struct {
-	ID           int `json:"article_id" gorm:"primary_key,AUTO_INCREMENT"`
-	Title        string
-	Body         string `sql:"type:text"`
-	ThumbnailURL string `json:"thumbnail_url"`
-	UserID       int
-	Tags         []*Tag  `gorm:"many2many:article_tags;"`
-	Shops        []*Shop `gorm:"many2many:article_shops;"`
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	ID           int       `json:"-" gorm:"primary_key,AUTO_INCREMENT"`
+	Title        string    `json:"title" sql:"not null"`
+	Body         string    `json:"body" sql:"not null;type:text"`
+	ThumbnailURL string    `json:"thumbnail_url" sql:"not null"`
+	UserID       int       `json:"-"`
+	Tags         []*Tag    `json:"tags" gorm:"many2many:article_tags;"`
+	Shops        []*Shop   `json:"shops" gorm:"many2many:article_shops;"`
+	CreatedAt    time.Time `json:"created_at,omitempty" gorm:"type:timestamp"`
+	UpdatedAt    time.Time `json:"updated_at,omitempty" gorm:"type:timestamp"`
 }
