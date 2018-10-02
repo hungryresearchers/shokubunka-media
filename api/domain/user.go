0259538fbf44a94a78d1c700081dbbfa26bb5e86
yasuno0327 <5315076@ed.tus.ac.jp>
2018-10-02 17:55:15 +0900

    fix

--- api/domain/user.go
+++ api/domain/user.go
@@ -7,14 +7,14 @@ import (
 
 type User struct {
 	ID                 int    `json:"id" gorm:"primary_key;AUTO_INCREMENT;not null"`
-	FirstName          string `json:"firstname" gorm:"not null" valid:"required"`
-	LastName           string `json:"lastname" gorm:"not null" valid:"required"`
+	FirstName          string `json:"firstname" gorm:"not null"`
+	LastName           string `json:"lastname" gorm:"not null"`
 	NickName           string `json:"nickname"`
-	Email              string `json:"email,omitempty" gorm:"unique_index;not null" binding:"exists,email" valid:"email,required"`
-	EncryptedPassword  string `json:"password,omitempty" gorm:"not null" valid:"required,length(8:255)"`
-	InvitationToken    string `json:"invitation_token,omitempty gorm:"unique_index;not null" valid:"required"`
+	Email              string `json:"email,omitempty" gorm:"unique_index;not null" binding:"exists,email"`
+	EncryptedPassword  string `json:"password,omitempty" gorm:"not null"`
+	InvitationToken    string `json:"invitation_token,omitempty gorm:"unique_index;not null"`
 	ResetPasswordToken string `json:"reset_password_token,omitempty"`
-	Role               *int   `json:"role,omitempty" gorm:"default:0;not null" valid:"required"` // role 0: user, 1: author, 2: admin
+	Role               *int   `json:"role,omitempty" gorm:"default:0;not null"` // role 0: user, 1: author, 2: admin
 	Articles           []Article
 	CreatedAt          time.Time `json:"created_at" gorm:"type:timestamp;DEFAULT:current_timestamp"`
 	UpdatedAt          time.Time `json:"updated_at" gorm:"type:timestamp;DEFAULT:current_timestamp"`
