04b4c2f5650e01ab312c758493b77bc11ef5ba13
yasuno0327 <5315076@ed.tus.ac.jp>
2018-10-04 21:43:03 +0900

    Implement helper method of currentuser

--- api/interfaces/controllers/context.go
+++ api/interfaces/controllers/context.go
@@ -1,10 +1,21 @@
 package controllers
 
-import "github.com/gin-gonic/gin/binding"
+import (
+	"api/domain"
+
+	"github.com/gin-gonic/gin/binding"
+)
 
 type Context interface {
 	Param(string) string
 	ShouldBindWith(interface{}, binding.Binding) error
 	Status(int)
+	MustGet(string) interface{}
 	JSON(int, interface{})
+}
+
+func CurrentUser(c Context) domain.User {
+	userInterface := c.MustGet("current_user")
+	currentUser := userInterface.(domain.User)
+	return currentUser
 }
